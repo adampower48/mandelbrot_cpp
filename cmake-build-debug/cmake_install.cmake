5f12e6ad34cdb474c07c234c23d85c90e6595c02
adampower48 <adam.power48@mail.dcu.ie>
2020-07-25 19:19:03 +0100

    Extend functionality to julia sets.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Adam/Documents/programming/c++/test
+# Install script for directory: C:/Users/Adam/Documents/programming/c++/mandelbrot
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Adam/Documents/programming/c++/test/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Adam/Documents/programming/c++/mandelbrot/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <complex>
-#include <iostream>
 #include <fstream>
 #include <opencv2/opencv.hpp>
+
+void displayImage(int height, int width, const int *data) {
+    using namespace cv;
+    int *cycledData = new int[height * width];
+    for (int i = 0; i < height * width; ++i) {
+        cycledData[i] = data[i] % 256;
+    }
+
+
+    Mat img(height, width, CV_32S, cycledData), mask;
+
+    img.convertTo(mask, CV_8U);
+    normalize(img, img, 0, 255, NORM_MINMAX, CV_32S, mask);
+
+    img.convertTo(img, CV_8U);
+    applyColorMap(img, img, COLORMAP_TWILIGHT_SHIFTED);
+
+    Mat img2(1080, 1080, CV_8U);
+    resize(img, img2, img2.size());
+
+    namedWindow("Display window", WINDOW_AUTOSIZE);
+    imshow("Display window", img2);
+    waitKey(0);
+}
 
 int mandelbrot(double x, double y, double bound, int maxIters) {
     using namespace std::complex_literals;
@@ -12,6 +35,10 @@ int mandelbrot(double x, double y, double bound, int maxIters) {
     if (p * (p + (x - 0.25)) <= 0.25 * y * y)
         return 0;
 
+
+    // Check for value inside 2nd bulb
+    if ((x + 1) * (x + 1) + y * y < 1. / 16)
+        return 0;
 
 
     std::complex<double> c = x + y * 1i;
@@ -46,6 +73,142 @@ void mandelbrotSquareArr(double centreX, double centreY, double size, int numSte
     }
 }
 
+void mandelbrotInplace(int i, double bound, std::complex<double> *c, std::complex<double> *curVals, int *iters,
+                       bool *active) {
+    curVals[i] = curVals[i] * curVals[i] + c[i];
+    iters[i]++;
+
+    if (std::norm(curVals[i]) >= bound * bound) {
+        active[i] = false;
+    }
+}
+
+void multibrotInplace(int i, double power, double bound, std::complex<double> *c, std::complex<double> *curVals,
+                      int *iters, bool *active) {
+    curVals[i] = pow(curVals[i], power) + c[i];
+    iters[i]++;
+
+    if (std::norm(curVals[i]) >= bound * bound) {
+        active[i] = false;
+    }
+}
+
+void juliaInplace(int i, double bound, std::complex<double> c, std::complex<double> *curVals,
+                  int *iters, bool *active) {
+    curVals[i] = curVals[i] * curVals[i] + c;
+    iters[i]++;
+
+    if (std::norm(curVals[i]) >= bound * bound) {
+        active[i] = false;
+    }
+}
+
+void juliasetSquareArr(double centreX, double centreY, double radius, int numSteps, double bound, double c_real,
+                       double c_im, int maxIters, int *outArr) {
+    double stepSize = 2 * radius / numSteps;
+    double xmin = centreX - radius;
+    double ymin = centreY - radius;
+
+
+    // Set up data arrays
+    auto c = std::complex(c_real, c_im);
+    auto *curVals = new std::complex<double>[numSteps * numSteps];
+    bool *active = new bool[numSteps * numSteps];
+    std::vector<int> activeIndices1;
+    std::vector<int> activeIndices2;
+
+    // Init data arrays
+    double x, y;
+    for (int i = 0; i < numSteps; ++i) {
+        for (int j = 0; j < numSteps; ++j) {
+            x = xmin + stepSize * j;
+            y = ymin + stepSize * i;
+
+            curVals[i * numSteps + j] = std::complex(x, y);
+            active[i * numSteps + j] = true;
+            activeIndices1.push_back(i * numSteps + j);
+        }
+    }
+
+    // Main iteration loop
+    for (int iter = 0; iter < maxIters; ++iter) {
+        for (int i : activeIndices1) {
+            juliaInplace(i, bound, c, curVals, outArr, active);
+
+            if (active[i]) {
+                activeIndices2.push_back(i);
+            }
+        }
+        std::swap(activeIndices1, activeIndices2);
+        activeIndices2.clear();
+    }
+
+    // Set still unknown values to 0
+    for (int i : activeIndices1) {
+        outArr[i] = 0;
+    }
+
+}
+
+void mandelbrotSquareArr2(double centreX, double centreY, double radius, int numSteps, double bound, int maxIters,
+                          int *outArr) {
+    double stepSize = 2 * radius / numSteps;
+    double xmin = centreX - radius;
+    double ymin = centreY - radius;
+
+    // Set up data arrays
+    auto *c = new std::complex<double>[numSteps * numSteps];
+    auto *curVals = new std::complex<double>[numSteps * numSteps];
+    bool *active = new bool[numSteps * numSteps];
+    std::vector<int> activeIndices1;
+    std::vector<int> activeIndices2;
+
+    // Init data arrays
+    double x, y, p;
+    for (int i = 0; i < numSteps; ++i) {
+        for (int j = 0; j < numSteps; ++j) {
+            x = xmin + stepSize * j;
+            y = ymin + stepSize * i;
+
+            c[i * numSteps + j] = std::complex(x, y);
+            curVals[i * numSteps + j] = 0;
+            active[i * numSteps + j] = true;
+            activeIndices1.push_back(i * numSteps + j);
+
+            // Check for main cardioid bulb
+            p = (x - 0.25) * (x - 0.25) + y * y;
+            if (p * (p + (x - 0.25)) <= 0.25 * y * y)
+                active[i * numSteps + j] = false;
+
+            // Check for 2nd cardioid bulb
+            if ((x + 1) * (x + 1) + y * y < 1. / 16)
+                active[i * numSteps + j] = false;
+
+        }
+    }
+
+    // Main iteration loop
+    for (int iter = 0; iter < maxIters; ++iter) {
+        for (int i : activeIndices1) {
+            mandelbrotInplace(i, bound, c, curVals, outArr, active);
+
+            if (active[i]) {
+                activeIndices2.push_back(i);
+            }
+        }
+        std::swap(activeIndices1, activeIndices2);
+        activeIndices2.clear();
+    }
+
+    // Set still unknown values to 0
+    for (int i : activeIndices1) {
+        outArr[i] = 0;
+    }
+
+
+}
+
+
 void writeArrToFile(int width, int height, int *outArr, const std::string &filename) {
     std::ofstream file;
     file.open(filename);
@@ -76,30 +239,33 @@ void writeArrToCout(int width, int height, int *outArr) {
 
 }
 
-void displayImage(int height, int width, int *data) {
-    using namespace cv;
-    Mat img(height, width, CV_32S, data), mask;
-    img.convertTo(mask, CV_8U);
-    normalize(img, img, 0, 255, NORM_MINMAX, CV_32S, mask);
-    img.convertTo(img, CV_8U);
-    applyColorMap(img, img, COLORMAP_VIRIDIS);
 
-    namedWindow("Display window", WINDOW_AUTOSIZE);
-    imshow("Display window", img);
-    waitKey(0);
-}
+int main(int argc, char **argv) {
+    // Parse args
+    std::string setType = argv[1];
+    int imgSteps = std::stoi(argv[2]);
+    int maxIters = std::stoi(argv[3]);
+    double centreX = std::stod(argv[4]);
+    double centreY = std::stod(argv[5]);
+    double radius = std::stod(argv[6]);
+    double bound = std::stod(argv[7]);
 
-int main() {
-    // Generate mandelbrot results
-    int n = 1000;
-    int *mandels = new int[n * n];
-    mandelbrotSquareArr(0.02445758009307159, 0.6296933276014198, 0.00001, n, 2, 10000, mandels);
+    // Calculate
+    int *mandels = new int[imgSteps * imgSteps]();
+    if (setType == "julia") {
+        // Julia
+        double juliaCReal = std::stod(argv[8]);
+        double juliaRIm = std::stod(argv[9]);
 
-//    writeArrToFile(n, n, mandels, "mandelbrot.csv");
-//    writeArrToCout(n, n, mandels);
+        juliasetSquareArr(centreX, centreY, radius, imgSteps, bound, juliaCReal, juliaRIm, maxIters, mandels);
+    } else {
+        // Mandelbrot
+        mandelbrotSquareArr2(centreX, centreY, radius, imgSteps, bound, maxIters, mandels);
+    }
 
-    displayImage(n, n, mandels);
-
+    // Output/show results
+    writeArrToFile(imgSteps, imgSteps, mandels, "mandelbrot.csv");
+    displayImage(imgSteps, imgSteps, mandels);
 
 
     return 0;
